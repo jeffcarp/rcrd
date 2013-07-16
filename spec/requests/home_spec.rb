f50174b876bd0064e9098e1c660085247fab319e
Jeff Carpenter <gcarpenterv@gmail.com>
2013-07-16 12:52:01 -0700

    Add test for guide_path

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe "Home" do
-  describe "GET /" do
+
+  describe "GET root_url" do
     it "loads" do
       get root_url 
+      response.status.should be(200)
+    end
+  end
+
+  describe "GET /guide (guide_path)" do
+    it "loads" do
+      get guide_path 
       response.status.should be(200)
     end
   end
