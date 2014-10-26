96ade848cee7361489d2f0feddda7bbe0c8bc89d
Jeff Carpenter <gcarpenterv@gmail.com>
2014-10-26 03:27:09 -0700

    Add naive cat search

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -5,4 +5,9 @@ class Cat < ActiveRecord::Base
   validates_format_of :color, 
     :with => /^(#[A-Fa-f0-9]{6}|#[A-Fa-f0-9]{3}|[01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$/,
     :message => "must be a valid hex code (like #e2235e) or a hue number from 0-255"
+
+  def self.search_cats(query)
+    Record.where("raw ILIKE ?", '%'+query+'%')
+  end
+
 end
---
+++ spec/models/cat_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+describe Cat do
+
+  describe "search_cats" do
+
+    it "finds exact matches" do
+      user = User.create!(
+        email: "whatever@jeff.is", 
+        password: "test", 
+        password_confirmation: "test",
+      )
+      one = user.records.create!(
+        raw: "foo, bar", 
+        target: Time.now,
+      )
+      two = user.records.create!(
+        raw: "bar, baz", 
+        target: Time.now,
+      )
+      expect(Cat.search_cats("bar")).to eq([two, one])
+    end
+
+  end
+
+end
