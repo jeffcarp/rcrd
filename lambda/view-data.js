44b15099c08d676edc6766aae06d6ae4e0cb204b
Jeff Carpenter <gcarpenterv@gmail.com>
2016-02-20 22:28:22 -0800

    Experiment with new cat page

---
+++ lambda/view-data.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function viewData(dynamo, params, context) {
+  if (!params.id) return context.fail('Missing param.id');
+
+  dynamo.getItem({
+    'TableName': 'rcrd-view-data',
+    'Key': {id: params.id}
+  }, function (err, record) {
+    if (err) {
+      context.fail(err);
+    } else {
+      context.succeed(record);
+    }
+  });
+}
+
+module.exports = viewData;
