3068989f29c363875664eea46c7aa8f3d235c689
Jeff Carpenter <gcarpenterv@gmail.com>
2016-02-22 20:22:44 -0800

    Set up mock server, this is excellent

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const bodyParser = require('body-parser')
+const context = require('./test/lambda/context-stub')
+const cors = require('cors')
+const express = require('express')
+const dynamoDocStub = require('./test/lambda/dynamodb-doc-stub')
+const fs = require('fs')
+const proxyquire = require('proxyquire').noCallThru()
 
+const app = express()
 const index = fs.readFileSync('index.html', 'utf-8');
+const lambda = proxyquire('./lambda/index', {
+  'dynamodb-doc': dynamoDocStub
+})
 
-app.use(express.static('.'));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('.'))
+
+seed()
+
+app.post('/api/*', function (req, res) {
+
+  context.callback = function (status, arg) {
+    console.log(status, arg)
+    res.json(arg);
+  };
+
+  lambda.handler(req.body, context);
+})
 
 app.get('*', function (req, res) {
   res.send(index);
@@ -14,3 +36,9 @@ app.listen(8000, function () {
   console.log('Listening on port 8000');
 });
 
+function seed() {
+  dynamoDocStub._setRecord({
+    id: '32423423',
+    raw: 'yas, yaaas'
+  })
+}
