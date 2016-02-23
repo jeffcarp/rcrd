2d85dce351f2ad9f9502e1564f64bae139007517
Jeff Carpenter <gcarpenterv@gmail.com>
2016-02-22 19:13:57 -0800

    Keep working on creating new records

--- lambda/create-record.js
+++ lambda/create-record.js
@@ -4,17 +4,37 @@ var crypto = require('crypto')
 var util = require('./util')
 
 function createRecord(dynamo, params, context) {
-  if (!params.raw || !params.time || !params.time_zone) {
-      context.fail('Missing param');
-  }
 
   if (params.id) {
-    var id = params.id
+    // Updating
+
+    var newRecord = {
+      id: params.id
+    }
+
+    if (params.raw) newRecord.raw = params.raw
+    if (params.time) newRecord.time = params.time
+    if (params.time_zone) newRecord.time_zone = params.time_zone
   } else {
+    // Creating
+
+    if (!params.raw || !params.time || !params.time_zone) {
+      context.fail('Missing param');
+    }
+
     // Add user_id to this
     var hashThis = params.time + params.raw
     var id = crypto.createHash('sha256').update(hashThis).digest('base64')
+
+    var newRecord = {
+      id: id,
+      raw: params.raw,
+      time: params.time,
+      time_zone: params.time_zone
+    }
   }
+
+
 
   var cats = util.catsFromRaw(params.raw); 
   if (util.hasDupes(cats.map(util.sansMagnitude))) {
@@ -22,12 +42,6 @@ function createRecord(dynamo, params, context) {
     return;
   }
   
-  var newRecord = {
-    id: id,
-    raw: params.raw,
-    time: params.time,
-    time_zone: params.time_zone
-  };
 
   dynamo.putItem({
     "TableName": "rcrd-records",
