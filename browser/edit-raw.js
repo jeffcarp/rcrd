8b0ddef6efb015b1491d577753eb78b97496e530
Jeff Carpenter <gcarpenterv@gmail.com>
2016-02-26 17:50:30 -0800

    Add also barebones time editing

--- browser/edit-raw.js
+++ browser/edit-raw.js
@@ -9,12 +9,22 @@ var EditRaw = React.createClass({
   getInitialState: function () {
     return {
       raw: '',
-      loading: false
+      loading: false,
+      timestamp: null,
     }
+  },
+
+  componentWillMount: function () {
+    this.setState({
+      timestamp: util.timeFromRecord(this.props.record),
+    })
   },
 
   render: function () {
     console.log(this.props)
+
+    var timestamp = this.state.timestamp.format('MMMM Do YYYY, h:mm:ss a');
+
     return (
       <form
         onSubmit={this.update}
@@ -32,18 +42,45 @@ var EditRaw = React.createClass({
           defaultValue={this.props.record.raw}
           disabled={this.state.loading}
           />
+        <div className="time m1-0b">{timestamp}</div>
+        <div className='m1-0b'>
+          <a onClick={this.augmentTime} data-num={-1} data-unit={'day'} className='button'>-1 day</a>
+          <a onClick={this.augmentTime} data-num={1} data-unit={'day'} className='button'>+1 day</a>
+          {' '}
+          <a onClick={this.augmentTime} data-num={-1} data-unit={'hours'} className='button'>-1 hours</a>
+          <a onClick={this.augmentTime} data-num={1} data-unit={'hours'} className='button'>+1 hour</a>
+          {' '}
+          <a onClick={this.augmentTime} data-num={-10} data-unit={'minutes'} className='button'>-10 min</a>
+          <a onClick={this.augmentTime} data-num={10} data-unit={'minutes'} className='button'>+10 min</a>
+        </div>
         <button
           onClick={this.resetRaw}
-          className='button'>Reset raw changes</button>
+          className='button'>Reset</button>
         <button
           onClick={this.update}
-          className='button'>Update raw</button>
+          className='button'>Update</button>
       </form>
     );
   },
 
   resetRaw: function () {
     this.refs.raw.value = this.props.record.raw
+    this.setState({
+      timestamp: util.timeFromRecord(this.props.record),
+    })
+  },
+
+  augmentTime: function (e) {
+    e.preventDefault()
+
+    var num = e.target.getAttribute('data-num');
+    var unit = e.target.getAttribute('data-unit');
+
+    this.setState({
+      timestamp: this.state.timestamp.add(num, unit)
+    })
+
+    return false;
   },
 
   update: function (e) {
@@ -54,6 +91,8 @@ var EditRaw = React.createClass({
     API.createRecord({
       id: this.props.record.id,
       raw: this.refs.raw.value,
+      time: this.state.timestamp.utc().format(),
+      time_zone: this.props.record.time_zone,
     }, function (err, data) {
       if (err) return console.error(err)
 
