e1198b8d5f9b603be68b7769c1fe3061b74694db
Jeff Carpenter <gcarpenterv@gmail.com>
2016-02-27 13:25:26 -0800

    wip - rcrd database migration from edge

---
+++ scripts/edge-migration.js
@@ -0,0 +1,53 @@
+const AWS = require('aws-sdk')
+const crypto = require('crypto')
+const doc = require('dynamodb-doc')
+const pg = require('pg')
+const moment = require('moment-timezone')
+
+const conString = 'postgres://jeff:password@localhost/rcrd_migration'
+AWS.config.update({region: 'us-east-1'})
+
+const dynamo = new doc.DynamoDB()
+
+pg.connect(conString, function(err, client, done) {
+  if (err) {
+    return console.error('error fetching client from pool', err);
+  }
+
+  console.log(moment.tz.zone('-480'), 'yeah')
+
+  client.query('SELECT * from records LIMIT 3', [], function(err, result) {
+    //call `done()` to release the client back to the pool
+    done()
+
+    if (err) {
+      return console.error('error running query', err);
+    }
+
+    result.rows.forEach(function (record, index) {
+      setTimeout(function () {
+
+        var time = moment.parseZone(record.target)
+/*
+        var hashThis = time + record.raw
+        var id = crypto.createHash('sha256').update(hashThis).digest('hex')
+
+
+        var newRecord = {
+          id: id,
+          raw: record.raw,
+          time: time,
+          time_zone: 'America/Los_Angeles'
+        }
+
+        console.log(newRecord)
+
+*/
+        if (index === result.rows.length - 1) {
+          client.end()
+        }
+      }, 1e3 * index * 1)  
+    })
+
+  });
+});
