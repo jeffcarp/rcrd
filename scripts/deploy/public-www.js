f776de163069a7161bc284d5188981293588463c
Jeff Carpenter <gcarpenterv@gmail.com>
2016-02-28 22:24:10 -0800

    Fix src/dst public-www S3 upload bug

--- scripts/deploy/public-www.js
+++ scripts/deploy/public-www.js
@@ -8,16 +8,17 @@ const BUCKET_NAME = 'www.rcrd.org';
 var s3bucket = new AWS.S3({params: {Bucket: BUCKET_NAME}});
 
 const files = [
-  ['www/index.html', 'text/html'],
+  ['www/index.html', 'index.html', 'text/html'],
 ];
 
 files.forEach(function (file) {
-  var filePath = file[0];
-  var contentType = file[1];
+  var srcFilePath = file[0];
+  var dstFilePath = file[1];
+  var contentType = file[2];
 
-  var str = fs.readFileSync(filePath, 'utf-8');
+  var str = fs.readFileSync(srcFilePath, 'utf-8');
   var params = {
-    Key: filePath, 
+    Key: dstFilePath, 
     Body: str,
     ContentType: contentType
   };
@@ -26,7 +27,7 @@ files.forEach(function (file) {
     if (err) {
       console.log("Error uploading data: ", err);
     } else {
-      console.log(`Successfully uploaded data to ${BUCKET_NAME}/${filePath}`);
+      console.log(`Successfully uploaded data to ${BUCKET_NAME}/${dstFilePath}`);
     }
   })
 })
