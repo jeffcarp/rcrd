6c7bb24a56b6d8f34c81a7092492c1d0a4fd24ac
Jeff Carpenter <gcarpenterv@gmail.com>
2016-03-01 12:27:02 -0800

    Delete all of Rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Nassau::Application.config.secret_token = 'b9a3d64f7d7414122c408ed0d519b5bfe90ce794e3672002db7606099b78d79d5e279aa396b234d2802ab4fb4a9b8dd3aa978a23200781a868b92d2ddb571a89'
--- db/migrate/20130705225033_add_default_state_to_cat_options.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultStateToCatOptions < ActiveRecord::Migration
-  def change
-    change_column :cats, :dashboard, :boolean, default: false
-    change_column :cats, :day_avgs, :boolean, default: false
-  end
-end
