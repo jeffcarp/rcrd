eb85b169998a78b8529b3e12f4410a5c6f1a299c
Jeff Carpenter <gcarpenterv@gmail.com>
2016-03-01 12:27:02 -0800

    Delete all of Rails

--- db/migrate/20130615221523_user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Migration
-  def change 
-    add_column :users, :password_hash, :string
-    add_column :users, :password_salt, :string
-  end
-end
--- db/migrate/20130810195421_add_color_to_cats.rb
+++
@@ -1,5 +0,0 @@
-class AddColorToCats < ActiveRecord::Migration
-  def change
-    add_column :cats, :color, :string
-  end
-end
