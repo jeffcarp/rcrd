c1cf1ff798eeefaf2411d6966a8aa8aa275f366d
Jeff Carpenter <gcarpenterv@gmail.com>
2016-03-24 20:34:20 -0700

    Add standard JS style and start migrating

--- test/lambda/update-record-test.js
+++ test/lambda/update-record-test.js
@@ -7,7 +7,7 @@ const tl = require('test-lambda')
 const dynamoDocStub = tl.dynamo
 const testLambda = tl.test(path.resolve('./lambda/index'), {
   before: require('./before'),
-  after: require('./after'),
+  after: require('./after')
 })
 const expectedID = '7056e94d4dd347c0408f8f6b661397af029363c54cf81af465c1d469f435f1b0'
 
@@ -15,11 +15,11 @@ test('updateRecord fails with no access token', function (t) {
   testLambda({
     operation: 'record.update'
   }, function (status, arg) {
-    t.equal(status, 'fail', 'status is fail');
-    t.equal(arg, 'access_token denied', 'error is "access_token denied"');
-    t.end();
-  });
-});
+    t.equal(status, 'fail', 'status is fail')
+    t.equal(arg, 'access_token denied', 'error is "access_token denied"')
+    t.end()
+  })
+})
 
 test('updateRecord fails with incorrect access token', function (t) {
   testLambda({
@@ -28,16 +28,16 @@ test('updateRecord fails with incorrect access token', function (t) {
   }, function (status, arg) {
     t.equal(status, 'fail', 'status is fail')
     t.equal(arg, 'access_token denied', 'error is "access_token denied"')
-    t.end();
-  });
-});
+    t.end()
+  })
+})
 
 test('updateRecord updates a record', function (t) {
-  dynamoDocStub._set('rcrd-records', { 
-    id: expectedID, 
-    raw: 'yas', 
-    time: '2016-05-22T18:19:19Z' ,
-    time_zone: 'America/Los_Angeles',
+  dynamoDocStub._set('rcrd-records', {
+    id: expectedID,
+    raw: 'yas',
+    time: '2016-05-22T18:19:19Z',
+    time_zone: 'America/Los_Angeles'
   })
 
   testLambda({
@@ -48,7 +48,7 @@ test('updateRecord updates a record', function (t) {
   }, function (status, data) {
     t.equal(status, 'succeed', 'status is succeed')
 
-    const actual = dynamoDocStub._get('rcrd-records', expectedID);
+    const actual = dynamoDocStub._get('rcrd-records', expectedID)
 
     t.ok(actual)
     t.equal(actual.id, expectedID)
--- test/lambda/validate-access-token-test.js
+++ test/lambda/validate-access-token-test.js
@@ -4,21 +4,19 @@ const path = require('path')
 const test = require('tape')
 const tl = require('test-lambda')
 
-const dynamoDocStub = tl.dynamo
 const testLambda = tl.test(path.resolve('./lambda/index'), {
   before: require('./before'),
-  after: require('./after'),
+  after: require('./after')
 })
-const expectedID = '7056e94d4dd347c0408f8f6b661397af029363c54cf81af465c1d469f435f1b0'
 
 test('expired tokens are rejected', function (t) {
   testLambda({
     operation: 'record.get',
     id: 'a-record-id',
-    access_token: 'expired_access_token',
+    access_token: 'expired_access_token'
   }, function (status, arg) {
-    t.equal(status, 'fail', 'status is fail');
-    t.equal(arg, 'access_token expired', 'error is "access_token denied"');
-    t.end();
-  });
-});
+    t.equal(status, 'fail', 'status is fail')
+    t.equal(arg, 'access_token expired', 'error is "access_token denied"')
+    t.end()
+  })
+})
