ccfd19b967f5a288220f1efae6e3653decd2273f
Jeff Carpenter <gcarpenterv@gmail.com>
2016-03-31 17:23:21 -0700

    Rename deploy scripts to make more sense

--- scripts/deploy/www.js
+++ scripts/deploy/www.js
@@ -4,22 +4,21 @@ var fs = require('fs')
 require('./confirm-correct-directory')()
 
 AWS.config.region = 'us-east-1'
-const BUCKET_NAME = 'app.rcrd.org'
+const BUCKET_NAME = 'www.rcrd.org'
 var s3bucket = new AWS.S3({params: {Bucket: BUCKET_NAME}})
 
 const files = [
-  ['index.html', 'text/html'],
-  ['static/index.js', 'application/javascript'],
-  ['static/style.css', 'text/css']
+  ['www/index.html', 'index.html', 'text/html']
 ]
 
 files.forEach(function (file) {
-  var filePath = file[0]
-  var contentType = file[1]
+  var srcFilePath = file[0]
+  var dstFilePath = file[1]
+  var contentType = file[2]
 
-  var str = fs.readFileSync(filePath, 'utf-8')
+  var str = fs.readFileSync(srcFilePath, 'utf-8')
   var params = {
-    Key: filePath,
+    Key: dstFilePath,
     Body: str,
     ContentType: contentType
   }
@@ -28,7 +27,7 @@ files.forEach(function (file) {
     if (err) {
       console.log('Error uploading data: ', err)
     } else {
-      console.log(`Successfully uploaded data to ${BUCKET_NAME}/${filePath}`)
+      console.log(`Successfully uploaded data to ${BUCKET_NAME}/${dstFilePath}`)
     }
   })
 })
