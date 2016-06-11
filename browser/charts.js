f0fd274514212dff78620f81d6914fb27b572b0a
Jeff Carpenter <gcarpenterv@gmail.com>
2016-06-11 10:28:11 -0700

    Expand graphs on index

--- browser/charts.js
+++ browser/charts.js
@@ -1,17 +1,24 @@
 var React = require('react')
 
-var GraphTimeSince = require('./graphs/graph-time-since')
+var GraphTimeSince = require('./graphs/time-since')
+var GraphWeekAverages = require('./graphs/week-averages')
 
 var Charts = React.createClass({
   render: function () {
     return (
       <div>
-        <GraphTimeSince
-          catName='haircut'
-          timeDivision='weeks'
+        <GraphWeekAverages
+          catName='floss'
+          last90Days={this.props.last90Days} />
+        <GraphWeekAverages
+          catName='workout'
           last90Days={this.props.last90Days} />
         <GraphTimeSince
           catName='run'
+          last90Days={this.props.last90Days} />
+        <GraphTimeSince
+          catName='haircut'
+          timeDivision='weeks'
           last90Days={this.props.last90Days} />
       </div>
     )
---
+++ browser/graphs/week-averages.js
@@ -0,0 +1,55 @@
+var Cat = require('../cat')
+var moment = require('moment')
+var React = require('react')
+var util = require('../util')
+
+var GraphWeekAverages = React.createClass({
+
+  propTypes: {
+    catName: React.PropTypes.string.isRequired,
+    last90Days: React.PropTypes.array.isRequired
+  },
+
+  averageSinceNumWeeksAgo: function (numWeeks) {
+    const startDate = moment().subtract(numWeeks, 'weeks')
+    const endDate = moment()
+
+    var total = 0
+    this.props.last90Days.forEach((record) => {
+      const cats = util.baseCatsFromRaw(record.raw)
+      if (cats.indexOf(this.props.catName) !== -1) {
+        const time = util.timeFromRecord(record)
+        if (time.isAfter(startDate) && time.isBefore(endDate)) {
+          total += 1
+        }
+      }
+    })
+
+    return Number(total / numWeeks).toFixed(2)
+  },
+
+  render: function () {
+    return (
+      <table className='chart'>
+        <tbody>
+          <tr>
+            <td>
+              <Cat name={this.props.catName} />
+            </td>
+            <td>
+              <div><b>{this.averageSinceNumWeeksAgo(16)}</b><small>/week last <b>16</b> weeks</small></div>
+            </td>
+            <td>
+              <div><b>{this.averageSinceNumWeeksAgo(8)}</b><small>/week last <b>8</b> weeks</small></div>
+            </td>
+            <td>
+              <div><b>{this.averageSinceNumWeeksAgo(4)}</b><small>/week last <b>4</b> weeks</small></div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+})
+
+module.exports = GraphWeekAverages
