e04bd13e0f4216cc6a0a3088802a38df90f70d18
Jeff Carpenter <gcarpenterv@gmail.com>
2016-08-22 19:42:12 -0700

    Replace www deploy script with app

--- scripts/deploy/www.js
+++ scripts/deploy/www.js
@@ -8,17 +8,18 @@ const BUCKET_NAME = 'www.rcrd.org'
 var s3bucket = new AWS.S3({params: {Bucket: BUCKET_NAME}})
 
 const files = [
-  ['www/index.html', 'index.html', 'text/html']
+  ['index.html', 'text/html'],
+  ['static/index.js', 'application/javascript'],
+  ['static/style.css', 'text/css']
 ]
 
 files.forEach(function (file) {
-  var srcFilePath = file[0]
-  var dstFilePath = file[1]
-  var contentType = file[2]
+  var filePath = file[0]
+  var contentType = file[1]
 
-  var str = fs.readFileSync(srcFilePath, 'utf-8')
+  var str = fs.readFileSync(filePath, 'utf-8')
   var params = {
-    Key: dstFilePath,
+    Key: filePath,
     Body: str,
     ContentType: contentType
   }
@@ -27,7 +28,7 @@ files.forEach(function (file) {
     if (err) {
       console.log('Error uploading data: ', err)
     } else {
-      console.log(`Successfully uploaded data to ${BUCKET_NAME}/${dstFilePath}`)
+      console.log(`Successfully uploaded data to ${BUCKET_NAME}/${filePath}`)
     }
   })
 })
